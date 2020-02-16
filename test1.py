96a8b3499e973ca9ac3d24ac6482c5e66bd5ac17
omega-Lee <2033211717@qq.com>
2020-02-16 18:14:02 +0800

    plot函数

---
+++ test1.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+# polt函数，画曲线
+def draw():
+    x = np.linspace(0, 10, 30)
+    print(x)
+    '''
+    plt.plot(x, y, format_string, **kwargs)
+    x轴数据，y轴数据，format_string控制曲线的格式字串
+    format_string
+    由颜色字符，风格字符，和标记字符
+    '''
+    plt.plot(x, np.sin(x), '-bo')
+    plt.show()
+
+
+if __name__ == "__main__":
+    draw()
