9cafcd26e4d391611c19a358f06cb42bac98396c
omega-Lee <2033211717@qq.com>
2020-02-16 18:23:52 +0800

    scatter基本参数设置

--- test2.py
+++ test2.py
@@ -3,9 +3,11 @@ import numpy as np
 
 
 def scatter():
-    data=np.linspace(0,10,30)
-    plt.scatter(data,np.sin(data))
+    data = np.linspace(0, 10, 30)
+    # scatter(x, y, 点的大小, 颜色，标记)
+    plt.scatter(data, np.sin(data), s=90, c='r', marker='x')
     plt.show()
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     scatter()
