2ad2c0f7d286be41469930598b028baa2b184493
omega-Lee <2033211717@qq.com>
2020-02-16 20:51:43 +0800

    误差数据

--- test4.py
+++ test4.py
@@ -10,6 +10,13 @@ if __name__ == "__main__":
     # 误差
     dy = 0.8
     y = np.sin(data) + dy * np.random.randn(50)
-
-    plt.errorbar(data, y, yerr=dy, fmt='.k')
+    # plt.errorbar(x, y,数据点的位置坐标
+    #              yerr=None, xerr=None,数据的误差范围
+    #              fmt='',数据点的标记样式以及相互之间连接线样式
+    #              ecolor=None,误差棒的线条颜色
+    #              elinewidth=None,误差棒的线条粗细
+    #              capsize=None,误差棒边界横杠的大小
+    #              capthick=None 误差棒边界横杠的厚度
+    #              )
+    plt.errorbar(data, y, yerr=dy, fmt='o', ecolor='r')
     plt.show()
