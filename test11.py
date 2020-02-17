707b5fc551d1a803f5b15ccd58ae178f9527b723
omega-Lee <2033211717@qq.com>
2020-02-17 16:26:37 +0800

    set xy label

---
+++ test11.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+if __name__ == '__main__':
+    x = np.linspace(0, 10, 100)
+    y = np.sin(x)
+    print(x)
+    plt.plot(x, y, ':')
+    # 设置y轴上下限
+    plt.ylim(-1.5,1.5)
+    # 设置x轴上下限
+    plt.xlim(-2,15)
+
+    # 设置xy轴标签
+    plt.xlabel('---X---')
+    plt.ylabel('---Y---')
+
+    plt.show()
