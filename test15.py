664252c25e14e0670e25e4fd7295e82d52ac1c30
omega-Lee <2033211717@qq.com>
2020-02-17 16:38:01 +0800

    参考区域

---
+++ test15.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# 绘制垂直于x轴x<4 and x>6的参考区域，以及y轴y<0.2 and y>-0.2的参考区域
+
+if __name__ == '__main__':
+    x = np.linspace(0, 10, 100)
+    y = np.sin(x)
+    print(x)
+    plt.plot(x, y, ':', label='sin(x)')
+    # 设置y轴上下限
+    plt.ylim(-1.5, 1.5)
+    # 设置x轴上下限
+    plt.xlim(-2, 15)
+
+    # 设置xy轴标签
+    plt.xlabel('---X---')
+    plt.ylabel('---Y---')
+
+    # 设置标题
+    plt.title('Sin Function')
+
+    # 设置网格
+    plt.grid()
+
+    # 设置参考区域:水平
+    plt.axvspan(xmin=4, xmax=6, facecolor='r', alpha=0.3)
+    # 设置参考区域:垂直
+    plt.axhspan(ymin=-0.2, ymax=0.2, facecolor='b', alpha=0.3)
+
+    plt.show()
