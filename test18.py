deb303c483bcd187f68ab71266910242f8f583ba
omega-Lee <2033211717@qq.com>
2020-02-17 16:58:57 +0800

    subplots

---
+++ test18.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# 在一张图里绘制sin,cos的图形，并展示图例
+
+if __name__ == '__main__':
+    x = np.linspace(0, 10, 1000)
+
+    # fig, ax = plt.subplots()等价于fig, ax = plt.subplots(11)
+    figure, ax = plt.subplots()
+
+    ax.plot(x, np.sin(x), label='sin')
+    ax.plot(x, np.cos(x), label='cos')
+
+    # 显示图例
+    ax.legend()
+
+    plt.show()
