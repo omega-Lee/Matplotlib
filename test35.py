3d435c59949b054c416ca322153ae15c14d318e4
omega-Lee <2033211717@qq.com>
2020-02-18 15:59:23 +0800

    螺线图+data

---
+++ test35.py
@@ -0,0 +1,20 @@
+from mpl_toolkits import mplot3d
+import matplotlib.pyplot as plt
+import numpy as np
+
+if __name__ == '__main__':
+    fig = plt.figure()
+    ax = plt.axes(projection='3d')
+
+    zline = np.linspace(0, 15, 1000)
+    xline = np.sin(zline)
+    yline = np.cos(zline)
+
+    ax.plot3D(xline, yline, zline)
+
+    zdata = 15 * np.random.random(100)
+    xdata = np.sin(zdata) + 0.1 * np.random.randn(100)
+    ydata = np.cos(zdata) + 0.1 * np.random.randn(100)
+    ax.scatter3D(xdata, ydata, zdata, c=zdata, cmap='Greens')
+
+    plt.show()
