bd77e82cdeefa01e605fdc8bacc420c110ba5706
omega-Lee <2033211717@qq.com>
2020-02-18 15:51:42 +0800

    3d图

---
+++ test33.py
@@ -0,0 +1,9 @@
+from mpl_toolkits import mplot3d
+import matplotlib.pyplot as plt
+import numpy as np
+
+if __name__ == '__main__':
+    fig = plt.figure()
+    ax = plt.axes(projection='3d')
+
+    plt.show()
